551c51f83e21a4d679a8d5c7b3e6a4877ef524e2
Mohammad Mashrafil Hossain Mahi <mashrafilmahi007@gmail.com>
2025-09-04 23:10:27 +0600

    Basic Admin Dashboard Setup Done [Admin]

---
+++ src/routes/adminSidebarItems.ts
@@ -0,0 +1,25 @@
+
+import UserDashboard from "../pages/User/UserDashboard";
+import UserProfile from "../pages/User/UserProfile";
+import type { ISidebarItem } from "../types";
+
+
+
+export const adminSidebarItems: ISidebarItem[] = [
+    
+  {
+    
+    items: [
+      {
+        title: "Dashboard",
+        url: "/user/dashboard",
+        component: UserDashboard
+      },
+      {
+        title: "Manage Profile",
+        url: "/user/profile",
+        component: UserProfile
+      },
+    ],
+  },
+];
--- src/utils/getSidebarItems.ts
+++ src/utils/getSidebarItems.ts
@@ -3,14 +3,15 @@
 import type { User } from "../types";
 import { role } from "../constants/role";
 import { userSidebarItems } from "../routes/userSidebarItems";
+import { adminSidebarItems } from "../routes/adminSidebarItems";
 
 
 export const getSidebarItems = (userRole: User["role"]) => {
   switch (userRole) {
     // case role.superAdmin:
     //   return [...adminSidebarItems];
-    // case role.admin:
-    //   return [...adminSidebarItems];
+    case role.admin:
+      return [...adminSidebarItems];
     case role.user:
       return [...userSidebarItems];
     default:
