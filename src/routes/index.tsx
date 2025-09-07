157a8b27a8b81652ae341b6e8020edf0e1673d8a
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2025-09-07 19:23:50 +0600

    Contact Page Design Added

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -14,6 +14,7 @@ import Unauthorized from "../utils/Unauthorized";
 import Home from "../pages/Home";
 import About from "../pages/About";
 import Features from "../pages/Features";
+import Contact from "../pages/Contact";
 
 
 
@@ -45,6 +46,10 @@ export const router = createBrowserRouter([
     {
       Component: Features,
       path:'/features'
+    },
+    {
+      Component: Contact,
+      path:'/contact'
     }
       
         
