11d98750602153093a1903b078708a4aec272028
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2025-09-11 17:07:27 +0600

    bug fixed

--- src/redux/store/store.authSlice.ts
+++ src/redux/store/store.authSlice.ts
@@ -1,37 +1,71 @@
-import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
-import Cookies from 'js-cookie';
-import type { User } from '../../types';
+// import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
+// import Cookies from 'js-cookie';
+// import type { User } from '../../types';
 
+
+// interface AuthState {
+//   user: User | null;
+//   token?: string | null;
+//   isAuthenticated: boolean;
+// }
+
+// const initialState: AuthState = {
+//   user: null,
+//   token: Cookies.get('token') || null,
+//   isAuthenticated: !!Cookies.get('token'),
+// };
+
+// const authSlice = createSlice({
+//   name: 'auth',
+//   initialState,
+//   reducers: {
+//     setCredentials: (state, action: PayloadAction<{ user: User; token: string  }>) => {
+//       state.user = action.payload.user;
+//       state.token = action.payload.token;
+//       state.isAuthenticated = true;
+      
+//       Cookies.set('token', action.payload.token, { expires: 7 });
+//     },
+//     logout: (state) => {
+//       state.user = null;
+//       state.token = null;
+//       state.isAuthenticated = false;
+      
+//       Cookies.remove('token');
+//     },
+//     updateUser: (state, action: PayloadAction<User>) => {
+//       state.user = action.payload;
+//     },
+//   },
+// });
+
+// export const { setCredentials, logout, updateUser } = authSlice.actions;
+// export default authSlice.reducer;
+
+import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
+import type { User } from '../../types';
 
 interface AuthState {
   user: User | null;
-  token?: string | null;
   isAuthenticated: boolean;
 }
 
 const initialState: AuthState = {
   user: null,
-  token: Cookies.get('token') || null,
-  isAuthenticated: !!Cookies.get('token'),
+  isAuthenticated: false,
 };
 
 const authSlice = createSlice({
   name: 'auth',
   initialState,
   reducers: {
-    setCredentials: (state, action: PayloadAction<{ user: User; token: string  }>) => {
+    setCredentials: (state, action: PayloadAction<{ user: User }>) => {
       state.user = action.payload.user;
-      state.token = action.payload.token;
       state.isAuthenticated = true;
-      
-      Cookies.set('token', action.payload.token, { expires: 7 });
     },
     logout: (state) => {
       state.user = null;
-      state.token = null;
       state.isAuthenticated = false;
-      
-      Cookies.remove('token');
     },
     updateUser: (state, action: PayloadAction<User>) => {
       state.user = action.payload;
